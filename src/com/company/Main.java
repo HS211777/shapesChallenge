3a2e103aa782b28e056725f1259e41a67590ac4d
HS211777 <90605684+HS211777@users.noreply.github.com>
2021-09-21 22:23:17 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,42 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number");
         int userInput = input.nextInt();
+        int spaceInsert = userInput;
+        int invSpaceInsert = 0;
         String output = "";
         int numberAdd = 1;
         int i;
+        int end;
         for (i = 0; i < userInput; i++){
+            output = output.replaceAll(" ","");
             String numberAdd1 = Integer.toString(numberAdd);
-            output = output + numberAdd1;
+            if (numberAdd1.equals("1")){
+                output = output + numberAdd1;
+            }
+            else{
+                output = numberAdd1 + output + numberAdd1;
+            }
+            end = 0;
+            while (end < spaceInsert){
+                output = " " + output;
+                end++;
+            }
             numberAdd += 1;
+            spaceInsert = spaceInsert - 1;
+            invSpaceInsert += 1;
             System.out.println(output);
         }
+        spaceInsert = 1;
         int length1;
         for (i = 0; i < userInput; i++){
+            spaceInsert += 1;
+            output = output.replaceAll(" ","");
             String numberAdd1 = Integer.toString(numberAdd);
             length1 = numberAdd1.length();
-            output = output.substring(0,output.length()-length1);
+            output = output.substring(length1,output.length()-length1);
+            for (i = 0; i < spaceInsert; i++){
+                output = " " + output;
+            }
             numberAdd -= 1;
             System.out.println(output);
         }
